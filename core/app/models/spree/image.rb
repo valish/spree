b8a1e8a8e5bd768d55377ca6778ab21bc523aa95
valish <andrew@vali.sh>
2015-04-19 05:19:37 -0700

    changing spree image path to shop

--- core/app/models/spree/image.rb
+++ core/app/models/spree/image.rb
@@ -5,8 +5,8 @@ module Spree
     has_attached_file :attachment,
                       styles: { mini: '48x48>', small: '100x100>', product: '240x240>', large: '600x600>' },
                       default_style: :product,
-                      url: '/spree/products/:id/:style/:basename.:extension',
-                      path: ':rails_root/public/spree/products/:id/:style/:basename.:extension',
+                      url: '/shop/products/:id/:style/:basename.:extension',
+                      path: ':rails_root/public/shop/products/:id/:style/:basename.:extension',
                       convert_options: { all: '-strip -auto-orient -colorspace sRGB' }
     validates_attachment :attachment,
       :presence => true,
